d79a00728c7e020a862649a71aac681b37a49936
demo_knyang <demo.knyang@gmail.com>
2021-12-02 00:31:55 +0800

    org elpa deprecated

--- publish.el
+++ publish.el
@@ -5,12 +5,12 @@
 (require 'package)
 (package-initialize)
 (unless package-archive-contents
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   ;; (add-to-list 'package-archives '("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/") t)
   ;; (add-to-list 'package-archives '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
   (package-refresh-contents))
-(dolist (pkg '(dash org-plus-contrib htmlize json-mode yaml-mode php-mode))
+(dolist (pkg '(org-contrib dash htmlize json-mode yaml-mode php-mode))
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
